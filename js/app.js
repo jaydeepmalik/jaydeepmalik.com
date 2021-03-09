2b18145962bd920a2c59da4bac85cd7098813ac3
Jaydeep Malik <34239755+jaydeepmalik@users.noreply.github.com>
2021-03-09 09:11:57 +0530

    update app.js

--- js/app.js
+++ js/app.js
@@ -139,25 +139,3 @@ $(document).ready(function() {
     });
   });
 
-  // ==============================================================================================
-  // Instagram Feed
-  // Get your userId and accessToken from the following URLs, then replace the new values with the
-  // the current ones.
-  // userId: https://smashballoon.com/instagram-feed/find-instagram-user-id/
-  // accessToken: http://instagram.pixelunion.net/
-  // ==============================================================================================
-
-  var instagramFeed = new Instafeed({
-    get: 'user',
-    limit: 6,
-    resolution: 'thumbnail',
-    userId: '441902919',
-    accessToken: '441902919.1677ed0.ab9040eff64249dcbfb19b6d48b4e909',
-    template:
-      '<div class="c-widget-instagram__item"><a href="{{link}}" title="{{caption}}" aria-label="{{caption}}" target="_blank"><img src="{{image}}"></a></div>'
-  });
-
-  if ($('#instafeed').length) {
-    instagramFeed.run();
-  }
-});
